6826f8b2004813effc701ac314112c4020a3168c
Jordi <jordi@iceverg.com>
2023-05-03 22:19:46 +0200

    ejemplos de modificadores skip, only y todo para escribir tests

--- demos/suma.test.js
+++ demos/suma.test.js
@@ -15,7 +15,10 @@ describe("funcion suma", () => {
 
     })
 
-    describe("suma de valores negativos", () => {
+    // .skip permite saltarnos tests o fragmentos de tests
+    // .only cambia la lógica de ejecución de jest (por defecto se ejecutan todos los tests)
+    // y ejecutará solamente aquellos tests que estén con .only
+    describe.skip("suma de valores negativos", () => {
         it("sumar -1 más -2 es igual a -3", () => {
             expect(suma(-1, -2)).toBe(-3)
         })
@@ -23,5 +26,10 @@ describe("funcion suma", () => {
         it("sumar -11 más -22 es igual a -33", () => {
             expect(suma(-11, -22)).toBe(-33)
         })
+
+    })
+    describe("sumar valores positivos y negativos", () => {
+        it.todo("sumar 1 y -1 es 0")
+
     })
 })
