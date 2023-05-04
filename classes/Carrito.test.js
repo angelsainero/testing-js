2d6a3b96bfbb10ab63f514c3caafdbd30cfa673b
Jordi <jordi@iceverg.com>
2023-05-04 21:25:19 +0200

    Refactorización del test usando hook beforeEach

--- classes/Carrito.test.js
+++ classes/Carrito.test.js
@@ -1,29 +1,26 @@
 import Carrito from './Carrito.js'
 
 describe('Testing class Carrito', () => {
+
+    let carrito
+    const nigiriItem = {
+        name: 'Nigiri',
+        price: 1.6
+    }
+    beforeEach(() => {
+        carrito = new Carrito()
+    })
     describe('Testing getTotalItems', () => {
         it('should return 0 at initialization', () => {
-            const carrito = new Carrito();
             expect(carrito.getTotalItems()).toEqual(0)
         })
         it('should return 1 after adding 1 product', () => {
-            const carrito = new Carrito();
-            carrito.addItem({
-                name: 'Nigiri',
-                price: 1.6
-            })
+            carrito.addItem(nigiriItem)
             expect(carrito.getTotalItems()).toBe(1)
         })
         it('should return 2 after adding 2 products', () => {
-            const carrito = new Carrito();
-            carrito.addItem({
-                name: 'Nigiri',
-                price: 1.6
-            })
-            carrito.addItem({
-                name: 'Nigiri',
-                price: 1.6
-            })
+            carrito.addItem(nigiriItem)
+            carrito.addItem(nigiriItem)
             expect(carrito.getTotalItems()).toBe(2)
         })
     })
