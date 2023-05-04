4f3ff2c1f9d8381722394796129cfb7ec6bb4c46
Jordi <jordi@iceverg.com>
2023-05-04 23:01:02 +0200

    Uso de mock de axios

--- classes/Users.test.js
+++ classes/Users.test.js
@@ -1,7 +1,14 @@
 import Users from './Users.js'
+import axios from 'axios'
+
+jest.mock('axios')
+
+// axios.get = () => {}
+// axios.post = () => {}
+// axios.put = () => {}
 
 describe('Users class', () => {
-    describe('Without mocking', () => {
+    describe.skip('Without mocking', () => {
         it('should return an array of 10 users', async () => {
             expect.assertions(2)
             try {
@@ -10,6 +17,37 @@ describe('Users class', () => {
                 expect(users).toHaveLength(10)
             }catch(e){
                 
+            }
+        })
+        it('should return username=Samantha for users[2]', async () => {
+            expect.assertions(1)
+            try {
+                const users = await Users.all()
+                expect(users[2].username).toMatch('Samantha')
+            }catch(e){   
+            }
+        })
+    })
+
+    describe('With mocks 👌', () => {
+        beforeAll(() => {
+            // configuración del mock
+            const users = [
+                {username: 'Jordi'}, 
+                {username: 'Aran'}, 
+                {username: 'Samantha'} 
+            ]
+            const res = {data: users}
+            axios.get.mockResolvedValue(res)
+        })
+        it('should return an array of users', async () => {
+            expect.assertions(2)
+            try {
+                const usersResponse = await Users.all()
+                expect(usersResponse).toBeArray()
+                expect(usersResponse).toHaveLength(3)
+            } catch(e) {
+
             }
         })
         it('should return username=Samantha for users[2]', async () => {
