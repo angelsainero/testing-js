4e409e2219204018d57975cd8556c1291c7fac37
Jordi <jordi@iceverg.com>
2023-05-05 19:37:45 +0200

    Más ejemplos de mocks con axios POST

--- classes/Users.js
+++ classes/Users.js
@@ -6,6 +6,12 @@ class Users {
     static all() {
         return axios.get(`${BASE_URL}/users`).then(response => response.data)
     }
+
+    static create(user) {
+        return axios
+            .post(`${BASE_URL}/users`, user)
+            .then(response => response.data)
+    }
 }
 
 module.exports = Users
--- classes/Users.test.js
+++ classes/Users.test.js
@@ -15,8 +15,8 @@ describe('Users class', () => {
                 const users = await Users.all()
                 expect(users).toBeArray()
                 expect(users).toHaveLength(10)
-            }catch(e){
-                
+            } catch (e) {
+
             }
         })
         it('should return username=Samantha for users[2]', async () => {
@@ -24,7 +24,7 @@ describe('Users class', () => {
             try {
                 const users = await Users.all()
                 expect(users[2].username).toMatch('Samantha')
-            }catch(e){   
+            } catch (e) {
             }
         })
     })
@@ -33,30 +33,47 @@ describe('Users class', () => {
         beforeAll(() => {
             // configuración del mock
             const users = [
-                {username: 'Jordi'}, 
-                {username: 'Aran'}, 
-                {username: 'Samantha'} 
+                { username: 'Jordi' },
+                { username: 'Aran' },
+                { username: 'Samantha' }
             ]
-            const res = {data: users}
+            const res = { data: users }
             axios.get.mockResolvedValue(res)
         })
-        it('should return an array of users', async () => {
-            expect.assertions(2)
-            try {
-                const usersResponse = await Users.all()
-                expect(usersResponse).toBeArray()
-                expect(usersResponse).toHaveLength(3)
-            } catch(e) {
+        describe('Users.all()', () => {
+            it('should return an array of users', async () => {
+                expect.assertions(2)
+                try {
+                    const usersResponse = await Users.all()
+                    expect(usersResponse).toBeArray()
+                    expect(usersResponse).toHaveLength(3)
+                } catch (e) {
 
-            }
+                }
+            })
+            it('should return username=Samantha for users[2]', async () => {
+                expect.assertions(1)
+                try {
+                    const users = await Users.all()
+                    expect(users[2].username).toMatch('Samantha')
+                } catch (e) {
+                }
+            })
         })
-        it('should return username=Samantha for users[2]', async () => {
-            expect.assertions(1)
-            try {
-                const users = await Users.all()
-                expect(users[2].username).toMatch('Samantha')
-            }catch(e){   
-            }
+        describe('Users.create()', () => {
+            it('should return the created user', async () => {
+                // configurar nuestro mock
+                const payload = {name: 'Billy'}
+                axios.post.mockResolvedValue({data: payload})
+                
+                expect.assertions(1)
+                try {
+                    const createdUser = await Users.create(payload)
+                    expect(createdUser).toMatchObject(payload)
+
+                }catch(e){}
+            })
         })
+
     })
 })
