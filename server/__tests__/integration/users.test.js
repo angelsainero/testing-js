9807771e27c062297e0100b3a26ad503f52e3d30
Jordi <jordi@iceverg.com>
2023-05-05 21:45:07 +0200

    Más ejemplos de tests de integración

--- server/__tests__/integration/users.test.js
+++ server/__tests__/integration/users.test.js
@@ -11,4 +11,22 @@ describe('Testing users endpoints', () => {
 
         })
     })
+
+    describe('GET /users/:id', () => {
+        it('should return a user', async () => {
+            expect.assertions(1)
+            const response = await request(app).get('/users/1')
+            expect(response.body).toHaveProperty('user')
+        })
+        it('should not return a 500 status code if id = -1', async () => {
+            expect.assertions(1)
+            const response = await request(app).get('/users/-1')
+            expect(response.statusCode).toBe(500)
+        })
+        it('should return user {id:1} for id = 1', async() => {
+            expect.assertions(1)
+            const response = await request(app).get('/users/1')
+            expect(response.body.user).toMatchObject({id: 1}) // podemos pasarle un objeto parcial, no hace falta todo el objeto entero
+        })
+    })
 })
